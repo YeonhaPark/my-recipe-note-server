fc23819054bc29101c0a120e2ec0fe280e177b67
Park Yeonha <49088833+YeonhaPark@users.noreply.github.com>
2022-01-06 15:01:40 -0600

    fix: modify cors option

--- app-route.js
+++ app-route.js
@@ -14,7 +14,11 @@ import db from './models/index.js';
 import './services/passport.js';
 
 const app = express();
-const whitelist = ['http://localhost:3000', 'http://127.0.0.1:3000'];
+const whitelist = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  'https://my-recipe-note-app.netlify.app',
+];
 
 const corsOptions = {
   origin: whitelist,
